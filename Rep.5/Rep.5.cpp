a1fbaf3ffaca3c78cafd9b2efbf44e57a01f2324
Олександр <goncars451@gmail.com>
2024-09-28 14:07:15 +0300

    Добавьте файлы проекта.

---
+++ Rep.5/Rep.5.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, ".1251");
+    int mark;
+    string meaning;
+    cout << "Введiть значення:";
+    cin >> mark;
+    if (mark < 60 && mark >= 0) {
+        cout << "Незараховано" << endl;
+    }
+    else if (mark >= 60 && mark <= 75) {
+        cout << "Зараховано" << endl;
+    }
+    else if (mark > 75 && mark <= 90) {
+        cout << "Добре" << endl;
+    }
+    else if (mark > 90 && mark <= 100) {
+        cout << "Відмінно" << endl;
+    }
+    else {
+        cout << "Невiрний ввiд" << endl;
+    }
+    return 0;
+}
